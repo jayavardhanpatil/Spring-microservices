fc0107370b86289e2e67c1317dfdf07d5a43b220
jayavardhanpatil <jayawardhan.patil@gmail.com>
2020-04-26 18:22:04 -0700

    Rating micro service

---
+++ movie-catalog-service/src/main/java/io/spring/moviecatalogservice/models/Movie.java
@@ -0,0 +1,30 @@
+package io.spring.moviecatalogservice.models;
+
+public class Movie {
+
+    String movieId;
+    String name;
+
+    Movie(){}
+
+    public Movie(String movieId, String name) {
+        this.movieId = movieId;
+        this.name = name;
+    }
+
+    public String getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(String movieId) {
+        this.movieId = movieId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ ratings-data-service/src/main/java/io/spring/ratingsdataservice/models/UserRating.java
@@ -0,0 +1,6 @@
+package io.spring.ratingsdataservice.models;
+
+public class UserRating {
+
+
+}
