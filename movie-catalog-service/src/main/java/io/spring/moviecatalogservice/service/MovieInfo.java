e3190664224562c424ca36be20bcbe2e45fc5e91
jayavardhanpatil <jayawardhan.patil@gmail.com>
2020-05-05 19:25:22 -0700

    Hystrix command

--- movie-catalog-service/src/main/java/io/spring/moviecatalogservice/service/MovieInfo.java
+++ movie-catalog-service/src/main/java/io/spring/moviecatalogservice/service/MovieInfo.java
@@ -1,6 +1,7 @@
 package io.spring.moviecatalogservice.service;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import io.spring.moviecatalogservice.models.CatalogItem;
 import io.spring.moviecatalogservice.models.Movie;
 import io.spring.moviecatalogservice.models.Rating;
@@ -15,7 +16,13 @@ public class MovieInfo {
     private RestTemplate restTemplate;
 
 
-    @HystrixCommand(fallbackMethod = "getFallBackCatalogItem")
+    @HystrixCommand(fallbackMethod = "getFallBackCatalogItem",
+                    commandProperties = {
+                        @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1000"),
+                            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "5"),
+                        @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "50"),
+                            @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "3000")
+                    })
     public CatalogItem getCatalogItem(Rating rating) {
         Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
         return new CatalogItem(movie.getName(), movie.getOverView(), rating.getRating());
